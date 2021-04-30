9cd860d7c3c1640b839c8b039f458abcb1c3e313
srpanchal <srpanchal2712@gmail.com>
2021-04-29 19:30:34 -0700

    remaining files

---
+++ npms/src/main/java/com/sjsu/npms/NpmsApplication.java
@@ -0,0 +1,13 @@
+package com.sjsu.npms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpmsApplication.class, args);
+	}
+
+}
